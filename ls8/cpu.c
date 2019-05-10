c0fc43d651e19e19e80f7f1ff687ea3370958c0e
CarloC24 <carlo.clamucha@gmail.com>
2019-05-10 10:57:09 -0500

    passing sprint

--- ls8/cpu.c
+++ ls8/cpu.c
@@ -20,9 +20,6 @@ unsigned char cpu_ram_read(struct cpu *cpu, unsigned char address)
 }
 
 void cpu_load(struct cpu *cpu, int argc, char *argv[])
-// #define LDI 0b10000010
-// #define HLT 0b00000001
-// #define PRN 0b01000111
 {
 
   char line[1024];
@@ -54,26 +51,6 @@ void cpu_load(struct cpu *cpu, int argc, char *argv[])
   fclose(fp);
 }
 
-//TODO OPEN FILE
-// char data[DATA_LEN] = {
-//     // From print8.ls8
-//     0b10000010, // LDI R0,8
-//     0b00000000,
-//     0b00001000,
-//     0b01000111, // PRN R0
-//     0b00000000,
-//     0b00000001 // HLT
-// };
-
-// int address = 0;
-
-// for (int i = 0; i < DATA_LEN; i++)
-// {
-//   cpu->ram[address++] = data[i];
-// }
-
-// TODO: Replace this with something less hard-coded
-
 /**
  * ALU
  */
@@ -83,13 +60,19 @@ void alu(struct cpu *cpu, enum alu_op op, unsigned char regA, unsigned char regB
   {
   case ALU_MUL:
     // TODO
+    // TODO: implement more ALU ops
     printf("MULTIPLYING \n");
     cpu->reg[regA] *= cpu->reg[regB];
     break;
-  default:
-    printf("Cant read your ALU instruction \n");
+  case ALU_ADD:
+    cpu->reg[regA] += cpu->reg[regB];
     break;
-    // TODO: implement more ALU ops
+  case ALU_CMP:
+    if (cpu->reg[regA] == cpu->reg[regB])
+    {
+      cpu->FLAG = 1;
+    }
+    break;
   }
 }
 
@@ -124,19 +107,18 @@ void cpu_run(struct cpu *cpu)
     {
     case HLT:
       running = 0;
-      printf("HALT!! \n\n");
+      printf("END \n\n");
       break;
     case PRN:
-      printf("Should be printing the argument %d \n", cpu->reg[operandA]);
+      printf("%d \n", cpu->reg[operandA]);
       break;
     case LDI:
-      // printf("In the LDI func \n ");
       cpu->reg[operandA] = operandB;
       break;
     case ADD:
+      alu(cpu, ALU_ADD, operandA, operandB);
       break;
     case MUL:
-      // printf("Multiply instruction \n");
       alu(cpu, ALU_MUL, operandA, operandB);
       break;
     case POP:
@@ -148,7 +130,6 @@ void cpu_run(struct cpu *cpu)
       cpu_ram_write(cpu, cpu->reg[SP], cpu->reg[operandA]);
       break;
     case CALL:
-      printf("inside call statement \n");
       //PUSH OUR SYS CALL IN THE STACK
       // PUSH THE CALL FUNCTION IN THE CURRENT STACK ADDRESS
       // TO BE EXECUTED BEFORE ANYTHING ELSE
@@ -156,28 +137,42 @@ void cpu_run(struct cpu *cpu)
       cpu_ram_write(cpu, cpu->reg[SP], cpu->PC + 1);
       // LETS EXECUTE PROCESS THE ON THE GIVEN REGISTER
       cpu->PC = cpu->reg[operandA] - 1;
-      printf("%d is the PC after the CALL statement \n \n", cpu->PC);
       break;
     case RET:
-      printf("inside ret statement \n");
       // RETURN TO THE SAVED WORK BY POPPING
       cpu->PC = cpu->ram[cpu->reg[SP]];
       //INCREMENT STACK POINTER
       cpu->reg[SP]++;
       break;
+    case CMP:
+      alu(cpu, ALU_CMP, operandA, operandB);
+      break;
+    case JMP:
+      cpu->PC = cpu->reg[operandA];
+      cpu->PC += 1;
+      break;
+    case JEQ:
+      if (cpu->FLAG == 1)
+      {
+        cpu->PC = cpu->reg[operandA];
+        cpu->PC -= 1;
+        break;
+      }
+      break;
+    case JNE:
+      if (cpu->FLAG == 0)
+      {
+        cpu->PC = cpu->reg[operandA];
+        cpu->PC -= 1;
+        break;
+      }
+      break;
     default:
-      printf("CANNOT READ INSTRUCTION!! \n");
       break;
     }
-    // printf("REGISTER %c \n", cpu->ram[cpu->reg[SP]]);
     // 6. Move the PC to the next instruction.
     cpu->PC += pc_to_be_added;
   }
-  //TEST for PUSH/POP
-  // for (int i = 0; i < 7; i++)
-  // {
-  //   printf("REGISTER %c \n", cpu->ram[cpu->reg[SP]]);
-  // }
 }
 
 /**
@@ -185,9 +180,10 @@ void cpu_run(struct cpu *cpu)
  */
 void cpu_init(struct cpu *cpu)
 {
+  // TODO: Initialize the PC and other special registers
   cpu->PC = 0;
+  cpu->FLAG = 0;
   memset(cpu->reg, 0, sizeof(cpu->reg));
   memset(cpu->ram, 0, sizeof(cpu->ram));
   printf("initialized cpu \n");
-  // TODO: Initialize the PC and other special registers
 }
